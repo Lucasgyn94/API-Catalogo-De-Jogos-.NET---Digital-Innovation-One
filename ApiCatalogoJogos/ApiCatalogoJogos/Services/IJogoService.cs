2e6c3f43bf221cdfd850b1e2e9dc1065e4386abb
Lucasgyn94 <needslucas944@gmail.com>
2021-12-18 15:05:20 -0300

    Criando um catalogo de jogos com boas práticas .NET, oferecido pela Digital Innovation One

---
+++ ApiCatalogoJogos/ApiCatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,25 @@
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
+
+// Uma tarefa (ou task) representa uma unidade de trabalho que deverá ser
+// realizada. Esta unidade de trabalho pode rodar em uma thread separada e
+// é também possível iniciar uma task de forma sincronizada a qual resulta
+// em uma espera pela thread chamada. Com tasks, você tem uma camada de
+// abstração mas também um bom controle sobre as threads relacionadas.
---
+++ ApiCatalogoJogos/ApiCatalogoJogos/ViewModel/JogoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.ViewModel
+{
+    public class JogoViewModel
+    {
+        // Um GUID é um número inteiro de 128 bits (16 bytes) que você pode usar em todos os computadores e redes sempre que um identificador exclusivo for necessário.
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+    }
+}
